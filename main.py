2651cca7cb88b10a0660c32f4c91e6a6f39675e6
maxspells <cory.jnson@gmail.com>
2023-06-20 20:37:00 -0400

    laying framework for spell preparing and calculations

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import sys
 sys.path.append('modules')
 import inspect
 import importlib
-
 import modules.spellbook as spellbook
+import modules.character as character
+from modules.character import chr
 from modules.spellbook import *
+
+book = [],[]
+prepared = [],[]
 
 def load_spells(module_name): # checks spellbook.py for classes. If it's a class, imports it and adds 
     module = importlib.import_module(module_name) # it to an array and returns array
@@ -14,11 +18,8 @@ def load_spells(module_name): # checks spellbook.py for classes. If it's a class
             list.append(obj)
     return list
 
-book = [],[]
-
 def storespell(i): #sorts spell into book array by spell level
     book[i.sp_lvl].append(i)
-
 
 def printout(z): #debug printing
     for x in book[z]:
@@ -26,8 +27,64 @@ def printout(z): #debug printing
         print(x.desc)
         print("_________________________________________________")
 
+def modifier(char):
+    mod = 0
+    match char.int:
+        case 6 | 7:
+            mod = -2
+        case 8 | 9:
+            mod = -1
+        case 10 | 11:
+            mod = 0
+        case 12 | 13:
+            mod = 1
+        case 14 | 15:
+            mod = 2
+        case 16 | 17:
+            mod = 3
+        case 18 | 19:
+            mod = 4
+        case 20 | 21:
+            mod = 5
+        case 22 | 23:
+            mod = 6
+        case 24 | 25:
+            mod = 7
+    return mod
+
+def bonusspells(char):#broken right now :(
+    bonus = [0,0,0,0,0,0,0,0,0,0]
+    match char.int:
+        case 12 | 13:
+            bonus = [0,1,0,0,0,0,0,0,0,0]
+        case 14 | 15:
+            bonus = [0,1,1,0,0,0,0,0,0,0]
+        case 16 | 17:
+            bonus = [0,1,1,1,0,0,0,0,0,0]
+        case 18 | 19:
+            bonus = [0,1,1,1,1,0,0,0,0]
+        case 20 | 21:
+            bonus = [0,2,1,1,1,1,0,0,0,0]
+        case 22 | 23:
+            bonus = [0,2,2,1,1,1,0,0,0,0]
+        case 24 | 25:
+            bonus = [0,2,2,2,1,1,1,1,0,0]
+    return bonus
+
+def perday(char):
+    spd = []
+    bonus = bonusspells(char)
+    core = char.spellsperday
+    for i in range(0-9):
+        x = char.spellsperday.index(i)
+        spd.append(core[x]+bonus[x])
+        print("index =",x)
+    return spd
+    
+
 allspells = load_spells('modules.spellbook')
 for i in allspells:
     storespell(i)
-printout(0)
-printout(1)
+
+sal = perday(chr)
+print(sal)
---
+++ modules/character.py
@@ -0,0 +1,9 @@
+class chr:
+    name = "Asha Greywynd"
+    str = 10
+    dex = 14
+    con = 12
+    int = 20
+    wis = 16
+    cha = 10
+    spellsperday = [0,4,2,1,0,0,0,0,0,0]
