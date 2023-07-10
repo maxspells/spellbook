3c1ff1631f96af32b21d188f33b4c8b17ed80a19
maxspells <cory.jnson@gmail.com>
2023-07-10 18:06:26 -0400

    removes redundant code, refactored api stuff

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ import sys
 import math
 import msvcrt as m
 sys.path.append('modules')
-import inspect
-import importlib
 import os
 import os.path
 import requests
-import json
 from modules.character import *
 newbook = []
 book = [[],[],[],[],[],[],[],[],[],[]]
@@ -43,34 +40,31 @@ def query_spell(spell,splvl,spellsleft):
                 query = True
     return x
 
+def splist_get(pc_class,level,page):
+    spelllist = requests.get(f"https://pfapi.whizkid.dev/api/Spell/Class/{pc_class}?level={level}&page={page}&limit=100").json()
+    return spelllist
+def splist_total(pc_class,level):
+    number = 0
+    for i in range(5):
+        number+=len(splist_get(pc_class,level,(i+1)))
+    return number
+
+
+
 def create_spellbook():
     for lvl in range(10):
         spellsleft = chr.spellsknown[lvl]
         if spellsleft>0:
-            list = requests.get(f"https://pfapi.whizkid.dev/api/Spell/Class/{chr.pc_class}?level={lvl}&page=1&limit=100").json()
-            list2 = requests.get(f"https://pfapi.whizkid.dev/api/Spell/Class/{chr.pc_class}?level={lvl}&page=2&limit=100").json()
-            list3 = requests.get(f"https://pfapi.whizkid.dev/api/Spell/Class/{chr.pc_class}?level={lvl}&page=3&limit=100").json()
+            totalspellsonlist = splist_total(chr.pc_class,lvl)
             spellnumber = 1
-            totalspellsonlist = (len(list)+len(list2)+len(list3))
-            for i in list:
-                if spellsleft > 0:
-                    art()
-                    print(f"You can pick {spellsleft} more level {lvl} spells. {spellnumber}/{totalspellsonlist}")
-                    spellsleft = query_spell(i,lvl,spellsleft)
-                    spellnumber+=1
-            for i in list2:
-                if spellsleft > 0:
-                    art()
-                    print(f"You can pick {spellsleft} more level {lvl} spells. {spellnumber}/{totalspellsonlist}")
-                    spellsleft = query_spell(i,lvl,spellsleft)
-                    spellnumber+=1
-            for i in list3:
-                if spellsleft > 0:
-                    art()
-                    print(f"You can pick {spellsleft} more level {lvl} spells. {spellnumber}/{totalspellsonlist}")
-                    spellsleft = query_spell(i,lvl,spellsleft)
-                    spellnumber+=1
-
+            for page in range(5):
+                splist = splist_get(chr.pc_class,lvl,(page+1))
+                for i in splist:
+                    if spellsleft > 0:
+                        art()
+                        print(f"You can pick {spellsleft} more level {lvl} spells. {spellnumber}/{totalspellsonlist}")
+                        spellsleft = query_spell(i,lvl,spellsleft)
+                        spellnumber+=1
 
 def searchspell(spell,property):
     sp = spell.replace(" ", "%20")
@@ -78,21 +72,6 @@ def searchspell(spell,property):
     obj = spell_obj.json()
     output = obj[f'{property}']
     return output
-
-
-# OUTDATED checks spellbook.py for classes. If it's a class, imports it and adds it to an array and returns array
-def load_spells(module_name): 
-    module = importlib.import_module(module_name)
-    list = []
-    for name, obj in inspect.getmembers(module):
-        if inspect.isclass(obj):
-            list.append(obj)
-    return list
-
-
-allspells = load_spells('modules.spellbook')
-def storespell(i): #sorts spell into book array by spell level
-    book[i.sp_lvl].append(i)
 
 def modifier(stat): #calculates intelligence modifier for spells that use it
     mod = math.floor((stat-10)/2)
@@ -111,44 +90,43 @@ def perday(): #calculates how many spells per day can be prepared per spell leve
         z+=1
     return spd
 
-#OUTDATED
-def writespells():
-    w = open("preparedspells.txt","w")
-    header = -1
-    for i in prepared:
-        if header < i.sp_lvl:
-            w.write("Level " + str(i.sp_lvl) + " spells" + "\n" + "-----------------" + "\n")
-            header = i.sp_lvl
-        w.write("-" + i.name + "-" + "\n" + "\n")
-        w.write(i.desc + "\n" + "\n")
-        w.write("\n")
-    w.close()
+# def writespells(): #NEEDS UPDATED FOR API SPELL LIST
+#     w = open("preparedspells.txt","w")
+#     header = -1
+#     for i in prepared:
+#         if header < i.sp_lvl:
+#             w.write("Level " + str(i.sp_lvl) + " spells" + "\n" + "-----------------" + "\n")
+#             header = i.sp_lvl
+#         w.write("-" + i.name + "-" + "\n" + "\n")
+#         w.write(i.desc + "\n" + "\n")
+#         w.write("\n")
+#     w.close()
 
-def prepare():
-    dex = 0
-    availableslots = perday(chr)
-    dex2 = 0
-    for ea in availableslots:
-        if ea > 0:
-            print(f"You can prepare {ea} level {dex2} spells.")
-            dex2+=1
-    wait()
-    art()
-    print(chr.name)
-    while(availableslots[dex]>0):
-        if (dex<=(len(book)-1)):
-            for spell in book[dex]:
-                if availableslots[dex]>0:
-                    art()
-                    print(chr.name)
-                    print(f"preparing level {spell.sp_lvl} spells, {availableslots[dex]} remaining.")
-                    if input(f"Do you want to prepare {spell.name}? y/n:") == "y":
-                        availableslots[dex] -= 1
-                        prepared.append(spell)
-        dex+=1
-        print(f"increase dex to {dex}")
-        if availableslots[dex] == 0:
-            continue
+# def prepare(): #NEEDS UPDATED FOR API SPELL LIST
+#     dex = 0
+#     availableslots = perday(chr)
+#     dex2 = 0
+#     for ea in availableslots:
+#         if ea > 0:
+#             print(f"You can prepare {ea} level {dex2} spells.")
+#             dex2+=1
+#     wait()
+#     art()
+#     print(chr.name)
+#     while(availableslots[dex]>0):
+#         if (dex<=(len(book)-1)):
+#             for spell in book[dex]:
+#                 if availableslots[dex]>0:
+#                     art()
+#                     print(chr.name)
+#                     print(f"preparing level {spell.sp_lvl} spells, {availableslots[dex]} remaining.")
+#                     if input(f"Do you want to prepare {spell.name}? y/n:") == "y":
+#                         availableslots[dex] -= 1
+#                         prepared.append(spell)
+#         dex+=1
+#         print(f"increase dex to {dex}")
+#         if availableslots[dex] == 0:
+#             continue
 def art():
     os.system('cls')
     print("""
@@ -159,35 +137,15 @@ def art():
  //________.|.________\\ 
 `----------`-'----------'
         Spellbook
-https://github.com/maxspells""")
+https://github.com/maxspells
+""")
 
 def wait():
     print("Press any key")
     m.getch()
 
-def start():
-    art()
-    wait()
-    art()
-    if (os.path.isfile("modules/spellbook.py")==True):
-        import modules.old_spellbook as old_spellbook
-        for i in allspells:
-            storespell(i)
-        print("spellbook.py found")
-    else:
-        print("No spellbook.py file found in modules folder")
-    if (os.path.isfile("modules/character.py")==True):
-        print("character.py found")
-    else:
-        print("character.py not found")   
-# start()
-# prepare()
-# writespells()
-# os.system('cls')
-create_spellbook()
-
 def writespellbook():  #REWORK THIS TO SAVE KNOWN SPELL ARRAY LIST
-    w = open("spells.txt","w")
+    w = open("spellbook.txt","w")
     header = -1
     for i in newbook:
         if header < i["classLevels"][chr.pc_class]:
@@ -202,4 +160,5 @@ def writespellbook():  #REWORK THIS TO SAVE KNOWN SPELL ARRAY LIST
         w.write("\n")
     w.close()
 
+create_spellbook()
 writespellbook()
