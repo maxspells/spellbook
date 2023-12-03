b160d690454e1ddcdd6ed019687062494fc91292
maxspells <cory.jnson@gmail.com>
2023-12-03 12:07:50 -0500

    moved api methods to pfapi module

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import msvcrt as m
 sys.path.append('modules')
 import os
 import os.path
-import requests
+from modules.pfapi import api
 from modules.character import *
 newbook = []
 book = [[],[],[],[],[],[],[],[],[],[]]
@@ -47,39 +47,20 @@ def query_spell(spell,splvl,spellsleft):
                 query = True
     return x
 
-def splist_get(pc_class,level,page):
-    spelllist = requests.get(f"https://pfapi.whizkid.dev/api/Spell/Class/{pc_class}?level={level}&page={page}&limit=100").json()
-    return spelllist
-
-def splist_total(pc_class,level):
-    number = 0
-    for i in range(5):
-        number+=len(splist_get(pc_class,level,(i+1)))
-    return number
-
 def create_spellbook():
     for lvl in range(10):
         spellsleft = chr.spellsknown[lvl]
         if spellsleft>0:
-            totalspellsonlist = splist_total(chr.pc_class,lvl)
+            totalspellsonlist = api.get_number_spells_perlvl(chr.pc_class,lvl)
             spellnumber = 1
             for page in range(5):
-                splist = splist_get(chr.pc_class,lvl,(page+1))
+                splist = api.splist_get(chr.pc_class,lvl,(page+1))
                 for i in splist:
                     if spellsleft > 0:
                         art()
                         print(f"You can pick {spellsleft} more level {lvl} spells. {spellnumber}/{totalspellsonlist}")
                         spellsleft = query_spell(i,lvl,spellsleft)
                         spellnumber+=1
-
-def searchspell(spell):
-    sp = spell.replace(" ", "%20")
-    spell_obj = requests.get(f"https://pfapi.whizkid.dev/api/Spell/{sp}")
-    if spell_obj.status_code == 404:
-        return spell_obj.status_code
-    else:
-        obj = spell_obj.json()
-        return obj
 
 def modifier(stat): #calculates intelligence modifier for spells that use it
     mod = math.floor((stat-10)/2)
@@ -98,43 +79,6 @@ def perday(): #calculates how many spells per day can be prepared per spell leve
         z+=1
     return spd
 
-# def writespells(): #NEEDS UPDATED FOR API SPELL LIST
-#     w = open("preparedspells.txt","w")
-#     header = -1
-#     for i in prepared:
-#         if header < i.sp_lvl:
-#             w.write("Level " + str(i.sp_lvl) + " spells" + "\n" + "-----------------" + "\n")
-#             header = i.sp_lvl
-#         w.write("-" + i.name + "-" + "\n" + "\n")
-#         w.write(i.desc + "\n" + "\n")
-#         w.write("\n")
-#     w.close()
-
-# def prepare(): #NEEDS UPDATED FOR API SPELL LIST
-#     dex = 0
-#     availableslots = perday(chr)
-#     dex2 = 0
-#     for ea in availableslots:
-#         if ea > 0:
-#             print(f"You can prepare {ea} level {dex2} spells.")
-#             dex2+=1
-#     wait()
-#     art()
-#     print(chr.name)
-#     while(availableslots[dex]>0):
-#         if (dex<=(len(book)-1)):
-#             for spell in book[dex]:
-#                 if availableslots[dex]>0:
-#                     art()
-#                     print(chr.name)
-#                     print(f"preparing level {spell.sp_lvl} spells, {availableslots[dex]} remaining.")
-#                     if input(f"Do you want to prepare {spell.name}? y/n:") == "y":
-#                         availableslots[dex] -= 1
-#                         prepared.append(spell)
-#         dex+=1
-#         print(f"increase dex to {dex}")
-#         if availableslots[dex] == 0:
-#             continue
 def art():
     os.system('cls')
     print("""
@@ -173,12 +117,9 @@ def search_func():
         art()
         print("Type q to return to menu")
         prompt = input("Search a spell:")
-        search = searchspell(prompt)
+        search = api.search_spell(prompt)
         if prompt == "q":
             break
-        elif search == 404:
-            print("Can't find spell")
-            wait()
         else:
             sp_desc(search)
 def main():
---
+++ modules/pfapi.py
@@ -0,0 +1,40 @@
+from requests import get
+
+class api:
+    @staticmethod
+    def splist_get(pc_class,level,page):
+        try:#TODO implement proper exception
+            spell_list = get(f"https://pfapi.whizkid.dev/api/Spell/Class/{pc_class}?level={level}&page={page}&limit=100").json()
+            return spell_list
+        except:
+            print("An exception has occured api.splist_get method")
+    
+    @staticmethod
+    def search_spell(spell):
+        sp = spell.replace(" ", "%20")
+        try:#TODO implement proper exception
+            spell_obj = get(f"https://pfapi.whizkid.dev/api/Spell/{sp}").json()
+            return spell_obj
+        except:
+            print ("An exception has occured api.search_spell method")
+
+    @staticmethod
+    def get_number_spells_perlvl(pc_class,level):
+        total_spells_allpages = 0
+        print(f"Searching for level {level} {pc_class} spells...")
+        for index in range(10):
+            number_on_page = len(api.splist_get(pc_class,level,index+1))
+           
+            if number_on_page <= 0:
+                break
+            else:                
+                total_spells_allpages += number_on_page
+                print(f"{total_spells_allpages} level {level} spells found")
+        return total_spells_allpages
+
+
+
+
+        
+
+
